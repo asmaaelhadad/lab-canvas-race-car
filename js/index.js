a4975152f959409bd5e73c06cda6b7c0258b5e49
asmaaelhadad <asmaaelhadad@gmail.com>
2023-01-19 17:23:33 +0100

    iteration 1,2,3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,55 @@
+
+
+const myCanvas = document.getElementById("canvas");
+const ctx = myCanvas.getContext("2d");
+myCanvas.style.border = "2px solid black";
+const height = myCanvas.height 
+const width = myCanvas.width
+const bgImg = new Image()
+bgImg.src = "images/road.png";
+
+let carX =100;
+let carY =260 ;
+let animateId ;
+let gameOver = false ;
+
+const car = new Image()
+car.src = "images/car.png";
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+ 
   };
+  const animate= () => {
+    if (gameOver == true){cancelAnimationFrame(animateId);}
+    else {
+animateId= requestAnimationFrame(animate);
+ }
 
-  function startGame() {}
+ ctx.drawImage(bgImg, 0, 0, myCanvas.width, myCanvas.height);
+ ctx.drawImage(car, carX,carY, 90, 120)
+
+document.addEventListener('keydown', event => {
+ 
+  if (event.key === 'ArrowLeft') {
+    // move paddle to the left
+    isMovingLeft = true ;
+    for (carX > 0){
+      carX -= 2;}
+  }
+  if (event.key === 'ArrowRight') {
+    // move paddle to the right
+    isMovingRight = true;
+    
+    for (carX < width){
+    carX += 2;
+  }}
+
+})
+
+
+  }
+
+  function startGame() {animate();}
 };
