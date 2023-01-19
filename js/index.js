be009d509bd3f39f9f0f963d0849b9f632db3c06
asmaaelhadad <asmaaelhadad@gmail.com>
2023-01-19 18:08:00 +0100

    iteration 1,2,3

--- js/index.js
+++ js/index.js
@@ -1,55 +1,62 @@
-
-
 const myCanvas = document.getElementById("canvas");
 const ctx = myCanvas.getContext("2d");
 myCanvas.style.border = "2px solid black";
-const height = myCanvas.height 
-const width = myCanvas.width
-const bgImg = new Image()
+const height = myCanvas.height;
+const width = myCanvas.width;
+const bgImg = new Image();
 bgImg.src = "images/road.png";
 
-let carX =100;
-let carY =260 ;
-let animateId ;
-let gameOver = false ;
+let carX = 100;
+let carY = 260;
+let animateId;
+let gameOver = false;
+let isMovingLeft = false;
+let isMovingRight = false;
 
-const car = new Image()
+const car = new Image();
 car.src = "images/car.png";
 
+const animate = () => {
+  ctx.drawImage(bgImg, 0, 0, myCanvas.width, myCanvas.height);
+  if (isMovingLeft) {
+    carX -= 5
+  
+  ctx.drawImage(car, carX, carY, 90, 120);
+}
+  if (isMovingRight) {
+    carX -= 5
+  
+  ctx.drawImage(car, carX, carY, 90, 120);
+}
+  if (gameOver == true) {
+    cancelAnimationFrame(animateId);
+  } else {
+    animateId = requestAnimationFrame(animate);
+  }
+};
+
+  function startGame() {
+    animate();
+  }
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
- 
   };
-  const animate= () => {
-    if (gameOver == true){cancelAnimationFrame(animateId);}
-    else {
-animateId= requestAnimationFrame(animate);
- }
 
- ctx.drawImage(bgImg, 0, 0, myCanvas.width, myCanvas.height);
- ctx.drawImage(car, carX,carY, 90, 120)
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      isMovingLeft = true;
+      
+    }
+    if (event.key === "ArrowRight") {
+      isMovingRight = true;
 
-document.addEventListener('keydown', event => {
- 
-  if (event.key === 'ArrowLeft') {
-    // move paddle to the left
-    isMovingLeft = true ;
-    for (carX > 0){
-      carX -= 2;}
-  }
-  if (event.key === 'ArrowRight') {
-    // move paddle to the right
-    isMovingRight = true;
-    
-    for (carX < width){
-    carX += 2;
-  }}
-
-})
+      /* for (carX < width){
+    carX ++ ;
+  }*/
+    }
+  });
 
 
-  }
-
-  function startGame() {animate();}
 };
